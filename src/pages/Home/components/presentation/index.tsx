43989bbb8e8f1ab326af14e91c3c5db28f9032ce
rickck11 <rick32377@gmail.com>
2023-01-03 17:59:48 -0300

    Presentation component from Home page finished

--- src/pages/Home/components/presentation/index.tsx
+++ src/pages/Home/components/presentation/index.tsx
@@ -1,42 +1,48 @@
-import { ShoppingCart } from "phosphor-react";
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
 import CoffeeDeliveryAside from "../../../../assets/CoffeeDeliveryAside.svg";
-import { PresentationContainer } from "../styles";
+import { Item, Items, PresentationContainer } from "./styles";
 
 export function Presentation() {
   return (
     <PresentationContainer>
       <section>
         <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-        <p>
-          Com o Coffe Delvery você recebe seu café onde estiver a qualquer hora
-        </p>
-        <div>
-          <div>
-            <div>
-              <ShoppingCart size={20} weight="fill" />
+        <h4>
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora
+        </h4>
+        <Items>
+          <div className="item-group">
+            <Item bgColor="yellow-dark">
+              <div className="icon">
+                <ShoppingCart size={20} weight="fill" />
+              </div>
               <p>Compra simples e segura</p>
-            </div>
-          </div>
+            </Item>
 
-          <div>
-            <div>
-              <ShoppingCart size={20} weight="fill" />
-              <p>Compra simples e segura</p>
-            </div>
+            <Item bgColor="yellow">
+              <div className="icon">
+                <Timer size={20} weight="fill" />
+              </div>
+              <p>Entrega rápida e rastreada</p>
+            </Item>
           </div>
-          <div>
-            <div>
-              <ShoppingCart size={20} weight="fill" />
-              <p>Compra simples e segura</p>
-            </div>
+          <div className="item-group">
+            <Item bgColor="base-text">
+              <div className="icon">
+                <Package size={20} weight="fill" />
+              </div>
+              <p>Embalagem mantém o café intacto</p>
+            </Item>
+
+            <Item bgColor="purple">
+              <div className="icon">
+                <Coffee size={20} weight="fill" />
+              </div>
+              <p>O café chega fresquinho até você</p>
+            </Item>
           </div>
-          <div>
-            <div>
-              <ShoppingCart size={20} weight="fill" />
-              <p>Compra simples e segura</p>
-            </div>
-          </div>
-        </div>
+        </Items>
       </section>
       <aside>
         <img src={CoffeeDeliveryAside} alt="" />
