c4326ae5ef21a80b8c75176c0746448705104f40
rickck11 <rick32377@gmail.com>
2023-01-03 14:52:32 -0300

    ESLint configuration @rocketseat pack

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,11 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended'
-	],
-	'overrides': [
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'react',
-		'@typescript-eslint'
-	],
-	'rules': {
-		'react/react-in-jsx-scope': 'off',
-		'linebreak-style': 0,
-		'indent': [
-			'error',
-			'tab'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		]
-	}
-}
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+  extends: ["@rocketseat/eslint-config/react"],
+};
